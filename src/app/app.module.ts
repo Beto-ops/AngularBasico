c33629038782f93144205fc83077bf6b5ccf5451
Humberto Rodrigues da Conceição Nagai <beto.r.conceicao@uni9.edu.br>
2021-01-18 22:45:32 -0300

    Adicionado rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './course/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './course/pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Componet } from './error-404/error-404.component';
+import { CourseInfoComponent } from './course/course-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Error404Componet,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: '**', component: Error404Componet
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
